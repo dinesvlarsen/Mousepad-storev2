7a27a10984799e67602979e385a28a15763c7244
Dines <dinesvlarsen@gmail.com>
2022-04-23 17:57:14 +0200

    styled the navigation

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -1,11 +1,30 @@
 <template>
-	<ul>
+	<ul class="navigation">
+		<RouterLink to="/">
+			<li>Logo</li>
+		</RouterLink>
 		<li>
-			<router-link to="/">til /</router-link>
-		</li>
-
-		<li>
-			<router-link to="/mousepad-1">til /mike</router-link>
+			<router-link to="/cart">
+				<div>{{ items }}</div>
+			</router-link>
 		</li>
 	</ul>
 </template>
+
+<style>
+.navigation {
+	display: flex;
+	justify-content: space-between;
+	padding: 26px 16px 26px;
+}
+</style>
+
+<script>
+export default {
+	computed: {
+		items() {
+			return this.$store.getters.cartItems.length;
+		},
+	},
+};
+</script>
