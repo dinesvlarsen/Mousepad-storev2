c1831009d05da2cd47e1136c87de6d1663256d36
Dines <dinesvlarsen@gmail.com>
2022-04-23 17:56:27 +0200

    Made it so items can be stored in store

--- app/src/store.js
+++ app/src/store.js
@@ -2,12 +2,44 @@ export default {
 	state() {
 		return {
 			data: [],
+			cart: [],
 		};
 	},
 
 	mutations: {
-		increment(state) {
-			state.count++
+		storeData(state, data) {
+			state.data = data
+		},
+
+		addToCart(state, product) {
+			const cartProduct = {
+				title: product.title,
+				price: product.price,
+				image: product.image.image.asset.url,
+			}
+			state.cart.push(cartProduct)
 		}
-	}
+	},
+
+	actions: {
+		storeData(storeData, data) {
+			storeData.commit('storeData', data)
+		},
+
+		addToCart(addToCart, product) {
+			addToCart.commit('addToCart', product)
+		}
+	},
+
+	getters: {
+		data(state) {
+			return state.data
+		},
+
+		cartItems(state) {
+			return state.cart
+		}
+	},
+
+
 };
