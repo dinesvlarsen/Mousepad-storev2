88860ae59159017998c74b24da2426de8df74799
dinesvlarsen <dinesvlarsen@gmail.com>
2022-04-24 22:02:32 +0200

    Added logic to keep count, and remove items

--- app/src/store.js
+++ app/src/store.js
@@ -3,6 +3,7 @@ export default {
 		return {
 			data: [],
 			cart: [],
+			totalInCart: 0,
 		};
 	},
 
@@ -12,12 +13,36 @@ export default {
 		},
 
 		addToCart(state, product) {
-			const cartProduct = {
-				title: product.title,
-				price: product.price,
-				image: product.image.image.asset.url,
+			// Checks if an item already exists in the store cart by looking through all the items in the cart array, and checking them against the current product id.
+			if (state.cart.some(item => item.id === product._id)) {
+				const alreadyInCart = state.cart.find(itemInCart => itemInCart.id === product._id);
+				alreadyInCart.count++
+			} else {
+
+				const cartProduct = {
+					title: product.title,
+					price: product.price,
+					image: product.image.image.asset.url,
+					id: product._id,
+					count: 1
+				}
+				state.cart.push(cartProduct)
 			}
-			state.cart.push(cartProduct)
+
+			state.totalInCart++
+		},
+
+		removeFromCart(state, product) {
+
+			const productToRemove = state.cart.find(itemInCart => itemInCart.id === product.id);
+
+			if (productToRemove.count === 1) {
+				state.cart.splice(productToRemove, 1)
+				state.totalInCart--
+			} else {
+				productToRemove.count--
+				state.totalInCart--
+			}
 		}
 	},
 
@@ -28,6 +53,10 @@ export default {
 
 		addToCart(addToCart, product) {
 			addToCart.commit('addToCart', product)
+		},
+
+		removeFromCart(removeFromCart, product) {
+			removeFromCart.commit('removeFromCart', product)
 		}
 	},
 
@@ -38,6 +67,10 @@ export default {
 
 		cartItems(state) {
 			return state.cart
+		},
+
+		totalInCart(state) {
+			return state.totalInCart
 		}
 	},
 
