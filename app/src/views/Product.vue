3254af0b0147c690885632813bbfbe68d27023f6
dinesvlarsen <dinesvlarsen@gmail.com>
2022-04-28 21:53:07 +0200

    added data to the product component

--- app/src/views/Product.vue
+++ app/src/views/Product.vue
@@ -1,6 +1,16 @@
 <template>
-	<div v-if="loading">...</div>
-	<pre v-else>result: {{ JSON.stringify(result, null, 3) }}</pre>
+	<div class="product-information">
+		<div v-if="loading">...</div>
+		<!-- Without this v-else the console throws a promise error, could never really figure out why -->
+		<div v-else>
+			<img :src="imageURL" :alt="result.image.alt" />
+			<h1>{{ result.title }}</h1>
+			<p>{{ result.description }}</p>
+			<button class="product__add-to-cart" @click="addToCart(product)">
+				Add To Cart
+			</button>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -11,6 +21,7 @@ export default {
 	mixins: [viewMixin],
 
 	async created() {
+		//this.$route.params.product slug is defined when we click the routerlink on home. We use this value to determine which product to query for with the GROQ slug variable.
 		await this.sanityFetch(query, {
 			slug: this.$route.params.productSlug,
 		});
@@ -20,7 +31,22 @@ export default {
 			image: this.result.image.image.asset.url,
 		});
 	},
+
+	computed: {
+		imageURL() {
+			return this.result.image.image.asset.url;
+		},
+	},
 };
 </script>
 
-<style></style>
+<style>
+/* Going to make this button into component or use slots, since with slots we can just keep reusing the style, even for the remove from cart button */
+.product__add-to-cart {
+	background-color: #191b1d;
+	color: white;
+	padding: 11px 43px;
+	border-radius: 5px;
+	margin-top: 16px;
+}
+</style>
