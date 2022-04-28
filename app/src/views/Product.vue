d22a42312dd4e3fd7f2681ac876a3b40e5d78c73
dinesvlarsen <dinesvlarsen@gmail.com>
2022-04-28 22:19:21 +0200

    removed projectpage and added some styling

--- app/src/views/Product.vue
+++ app/src/views/Product.vue
@@ -1,12 +1,19 @@
 <template>
-	<div class="product-information">
+	<div>
 		<div v-if="loading">...</div>
 		<!-- Without this v-else the console throws a promise error, could never really figure out why -->
-		<div v-else>
-			<img :src="imageURL" :alt="result.image.alt" />
+		<div v-else class="product-information">
+			<img
+				class="product-information__image"
+				:src="imageURL"
+				:alt="result.image.alt"
+			/>
 			<h1>{{ result.title }}</h1>
 			<p>{{ result.description }}</p>
-			<button class="product__add-to-cart" @click="addToCart(product)">
+			<button
+				class="product-information__add-to-cart"
+				@click="addToCart(product)"
+			>
 				Add To Cart
 			</button>
 		</div>
@@ -32,6 +39,12 @@ export default {
 		});
 	},
 
+	methods: {
+		addToCart(product) {
+			this.$store.dispatch('addToCart', product);
+		},
+	},
+
 	computed: {
 		imageURL() {
 			return this.result.image.image.asset.url;
@@ -41,8 +54,14 @@ export default {
 </script>
 
 <style>
+.product-information *:not(.product-information__image) {
+	margin: 0 32px;
+}
+.product-information {
+	margin-top: 48px;
+}
 /* Going to make this button into component or use slots, since with slots we can just keep reusing the style, even for the remove from cart button */
-.product__add-to-cart {
+.product-information__add-to-cart {
 	background-color: #191b1d;
 	color: white;
 	padding: 11px 43px;
