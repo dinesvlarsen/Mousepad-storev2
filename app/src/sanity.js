bd753cb61a9b7a145fd69aae1b525e09b5ee402b
dinesvlarsen <dinesvlarsen@gmail.com>
2022-05-31 17:01:38 +0200

    added useCdn true

--- app/src/sanity.js
+++ app/src/sanity.js
@@ -3,6 +3,7 @@ import sanityClient from '@sanity/client';
 export default sanityClient({
 	projectId: 'w9b51wro',
 	dataset: 'production',
-	apiVersion: '2022-04-19', //dagen prosjektet ble først opprettet/sist oppdatert
-	useCdn: false //false for localhost, true for netlify
+	apiVersion: '2022-04-19', //day the project was started
+	useCdn: true, //false for localhost, true for netlify
+	// withCredentials: true
 });
